33abfd9b56fab5b2e47b89f462f17745c4f1f0d2
ahmedjilani123 <ahmedjilani0011@gmail.com>
2025-03-12 14:43:48 +0530

    add css and design

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "ca/covidapp/model/models"
-], (UIComponent, models) => {
+    "ca/covidapp/model/models",
+    "sap/ui/model/json/JSONModel"
+], (UIComponent, models,JSONModel) => {
     "use strict";
 
     return UIComponent.extend("ca.covidapp.Component", {
@@ -18,7 +19,26 @@ sap.ui.define([
 
             // set the device model
             this.setModel(models.createDeviceModel(), "device");
-
+            var data =[
+                {
+                    label:"A",
+                    value:12
+                },
+                {
+                    label:"B",
+                    value:12
+                },
+                {
+                    label:"C",
+                    value:12
+                },
+                {
+                    label:"D",
+                    value:12
+                },
+            ]
+var model = new JSONModel(data);
+this.setModel(model,"change");
             // enable routing
             this.getRouter().initialize();
         }
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,69 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/BusyIndicator"
+], (Controller,BusyIndicator) => {
     "use strict";
 
     return Controller.extend("ca.covidapp.controller.View1", {
-        onInit() {
+        dataPath:"https://api.rootnet.in/covid19-in/stats/latest",
+        onInit () {
+            // BusyIndicator.show();
+            var oModel = new sap.ui.model.json.JSONModel();
+            oModel.loadData(this.dataPath); 
+            this.getView().setModel(oModel, "covidData");
+            oModel.attachRequestCompleted(()=>{
+                 BusyIndicator.hide();
+             var pieChart =[];
+                oModel.getData().data.regional.forEach((item,i)=>{
+if(i<4){
+    pieChart.push({
+        "loc":item.loc,
+        "Cases":item.totalConfirmed
+    });
+}
+                });
+             oModel.setProperty("/pie",pieChart);
+            });
+
+            var vizframe = this.getView().byId("vizframeId");
+            vizframe.setVizProperties({
+                title: {
+                    text: "4 Main States"
+                },
+                plotArea: {
+                    colorPalette: ["#F99417","#d3321d","#f756aa","#a106f4"],
+                    drawingEffect: "glossy",
+                    background:{
+                        color:"#cad0ff"
+                    }
+                },
+                legendGroup: {
+                    layout: {
+                        position: "bottom"
+                    },
+                    computedVisibility:false,
+                    forceToShow:false
+                },
+                general:{
+                    background:{
+                        color:"#cad0ff"
+                    }
+                }
+            })
+        },
+        getAllData:function(){
+            var data = this.getView().getModel("covidData").getData();
+            this.Dialog ??= new sap.ui.xmlfragment("ca.covidapp.fragment.allData", this);
+            this.getView().addDependent(this.Dialog);
+
+            this.Dialog.open();
+        },
+        CloseDialogPress:function(oEvent){
+            oEvent.getSource().getParent().getParent().close();
+            this.Dialog=undefined;
+        },
+        AvatarPress:function(){
+            sap.m.MessageToast.show("Avatar Pressed");
         }
     });
 });
